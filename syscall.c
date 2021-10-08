ef4e5004463b93b4779e4fea22f889c21c774cd6
gole <b_igboss@163.com>
2021-10-08 17:29:42 +0800

    fixbug

--- syscall.c
+++ syscall.c
@@ -174,3 +174,18 @@ syscall(void)
   }
 
 }
+
+int
+sys_alarm(void)
+{
+    int ticks;
+    void (*handler)();
+
+    if(argint(0, &ticks) < 0)
+        return -1;
+    if(argptr(1, (char**)&handler, 1) < 0)
+        return -1;
+    myproc()->alarmticks = ticks;
+    myproc()->alarmhandler = handler;
+    return 0;
+}
